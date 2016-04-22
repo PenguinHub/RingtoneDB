660b9d15b78b95812234772b8fbb926e6c7f2213
yoni <yoni666@gmail.com>
2016-04-22 10:58:33 -0400

    finished skeleton

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,7 +1,15 @@
 package main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by yoni_ on 2016-04-20.
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
